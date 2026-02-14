c6f5a0d87d53eecfb57fbef39d98bac886d93ae8
MeowCatpawkittie <sor20704@gmail.com>
2026-02-15 00:35:58 +0900

    Update widget.c

--- src/widget.c
+++ src/widget.c
@@ -123,8 +123,7 @@ static void indicate_connectivity_internal(void) {
     struct blink_item blink = {.duration_ms = CONFIG_RGBLED_WIDGET_CONN_BLINK_MS};
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    struct zmk_endpoint_instance ep = zmk_endpoint_get_selected();
-switch (ep.transport) {
+    switch (zmk_endpoint_get_selected().transport) {
     case ZMK_TRANSPORT_USB:
 #if IS_ENABLED(CONFIG_RGBLED_WIDGET_CONN_SHOW_USB)
         LOG_INF("USB connected, blinking %s", color_names[CONFIG_RGBLED_WIDGET_CONN_COLOR_USB]);
